99e6dbd57eb78157cf8f15e699b75b7e0d25bf8b
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-01-27 02:17:58 +0530

    backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs=require("fs");
+
+// fs.mkdirSync("akshay")
+
+// fs.writeFileSync("bio.text", "im akshay singh rajput")
+
+// const data=fs.readFileSync("bio.text", "utf8")
+// console.log(data)
+
+// fs.unlinkSync
+
+// fs.renameSync("bio.text", "sonu.text")
+
+// fs.rmdirSync("akshay")
+
+// fs.unlinkSync("index.js")
+
+// fs.rmdirSync("index")
+
+fs.writeFileSync("index.js")
