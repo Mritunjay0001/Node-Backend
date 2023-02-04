8890cc75d66a2bc8d78e2a912d32688a2b1c2219
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-02-05 01:35:08 +0530

    express

--- express/src/index.js
+++ express/src/index.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
+// console.log(__dirname)
+
+const staticPath= path.join(__dirname, "../public")
+app.use(express.static(staticPath))
 app.get("/", (req, res) => {
     res.send("hello world!");
 });
